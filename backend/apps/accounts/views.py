bdbecb8882a978ea53d046da0cb870961b3ec450
Marcos Santos <krzgvfs@gmail.com>
2025-10-18 17:59:20 -0300

    feat(auth): implements the basic endpoints for authentication
    - Custom endpoint for user registration
    - Login endpoint using the standard TokenObtainPairView view
    - Session renewal endpoint using the standard TokenRefreshView view

--- backend/apps/accounts/views.py
+++ backend/apps/accounts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from .serializers import UserSerializer
+
+
+class RegisterView(generics.CreateAPIView):
+    """
+    API endpoint for user registration.
+    """
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = UserSerializer
